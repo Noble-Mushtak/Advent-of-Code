c8e794d1508e4d9b2267b1a67e7661f9f7401cab
Noble-Mushtak <noblemushtak.public@gmail.com>
2020-12-24 13:28:09 -0500

    Added days 9 and 10

---
+++ 2020/day09/src/lib.rs
@@ -0,0 +1,102 @@
+use std::cmp::Ordering;
+use std::error::Error;
+use std::fs;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug)]
+struct XmasData(Vec<usize>);
+
+impl FromStr for XmasData {
+    type Err = ParseIntError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        Ok(XmasData(
+            input
+                .trim()
+                .split('\n')
+                .map(str::parse)
+                .collect::<Result<_, _>>()?,
+        ))
+    }
+}
+
+impl XmasData {
+    fn find_first_invalid(&self, &preamble_len: &usize) -> Option<usize> {
+        (preamble_len..self.0.len()).find(|&i| {
+            let slice = &self.0[i - preamble_len..i];
+            let desired_val = self.0[i];
+            !slice
+                .iter()
+                .map(|x| slice.iter().map(move |y| (x, y)))
+                .flatten()
+                .any(|(x, y)| x + y == desired_val)
+        })
+    }
+
+    fn find_slice_with_sum(&self, &desired_sum: &usize) -> Option<&[usize]> {
+        for i in 0..self.0.len() {
+            let mut sum = 0;
+            for j in i..self.0.len() {
+                sum += self.0[j];
+                match sum.cmp(&desired_sum) {
+                    Ordering::Equal => return Some(&self.0[i..j + 1]),
+                    Ordering::Greater => continue,
+                    Ordering::Less => (),
+                };
+            }
+        }
+        None
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let data: XmasData = fs::read_to_string("in.txt")?.parse()?;
+
+    let answer1 = data.0[data
+        .find_first_invalid(&25)
+        .expect("Answer for part 1 not found")];
+    println!("Part 1: {}", answer1);
+
+    let desired_slice = data
+        .find_slice_with_sum(&answer1)
+        .expect("Answer for part 2 not found");
+    println!(
+        "Part 2: {}",
+        desired_slice.iter().min().expect("Slice can not be empty")
+            + desired_slice.iter().max().expect("Slice can not be empty")
+    );
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_data() -> XmasData {
+        XmasData(vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ])
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!("1\n2\n3\n".parse(), Ok(XmasData(vec![1, 2, 3])));
+        assert!("1\n2\ndfg\n".parse::<XmasData>().is_err());
+    }
+
+    #[test]
+    fn test_invalid() {
+        let data = example_data();
+        let invalid_idx = data.find_first_invalid(&5).unwrap();
+        assert_eq!(data.0[invalid_idx], 127);
+    }
+
+    #[test]
+    fn test_slice() {
+        let data = example_data();
+        assert_eq!(data.find_slice_with_sum(&127), Some(&[15, 25, 47, 40][..]));
+    }
+}
---
+++ 2020/day10/src/lib.rs
@@ -0,0 +1,118 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug)]
+struct JoltageData(Vec<usize>);
+
+impl FromStr for JoltageData {
+    type Err = ParseIntError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        Ok(JoltageData(
+            input
+                .trim()
+                .split('\n')
+                .map(str::parse)
+                .collect::<Result<_, _>>()?,
+        ))
+    }
+}
+
+impl JoltageData {
+    fn relative_diff_distribution(&mut self) -> HashMap<usize, usize> {
+        self.0.sort_unstable();
+
+        let mut distribution = HashMap::new();
+        for i in 1..self.0.len() {
+            let diff = self.0[i] - self.0[i - 1];
+            assert!((1..=3).contains(&diff));
+
+            match distribution.get_mut(&diff) {
+                Some(val) => *val += 1,
+                None => {
+                    distribution.insert(diff, 1);
+                }
+            };
+        }
+        distribution
+    }
+
+    fn count_arrangements(&self) -> usize {
+        //Assume sorted
+
+        let mut num_arrangements = vec![1];
+        for i in 1..self.0.len() {
+            num_arrangements.push(
+                (0..i)
+                    .rev()
+                    .take_while(|&j| self.0[i] - self.0[j] <= 3)
+                    .map(|j| num_arrangements[j])
+                    .sum(),
+            );
+        }
+        num_arrangements[self.0.len() - 1]
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let mut data: JoltageData = fs::read_to_string("in.txt")?.parse()?;
+    data.0.push(0);
+
+    let distribution = data.relative_diff_distribution();
+    let num1s = distribution.get(&1).unwrap_or(&0);
+    let num3s = distribution.get(&3).unwrap_or(&0) + 1;
+    println!("Part 1: {}", num1s * num3s);
+    println!("Part 2: {}", data.count_arrangements());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!("1\n2\n3\n".parse(), Ok(JoltageData(vec![1, 2, 3])));
+        assert!("1\n2\ndfg\n".parse::<JoltageData>().is_err());
+    }
+
+    fn example_data1() -> JoltageData {
+        JoltageData(vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4])
+    }
+
+    fn example_data2() -> JoltageData {
+        JoltageData(vec![
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35,
+            8, 17, 7, 9, 4, 2, 34, 10, 3,
+        ])
+    }
+
+    #[test]
+    fn test_calc_distr() {
+        let mut data1 = example_data1();
+        assert_eq!(
+            data1.relative_diff_distribution(),
+            [(1, 6), (3, 4)].iter().cloned().collect()
+        );
+        let mut data2 = example_data2();
+        assert_eq!(
+            data2.relative_diff_distribution(),
+            [(1, 21), (3, 9)].iter().cloned().collect()
+        );
+    }
+
+    #[test]
+    fn test_calc_distr() {
+        let mut data1 = example_data1();
+        data1.0.sort_unstable();
+        assert_eq!(data1.count_arrangements(), 8);
+
+        let mut data2 = example_data2();
+        data2.0.sort_unstable();
+        assert_eq!(data2.count_arrangements(), 19208);
+    }
+}

6830849ff2428deb6cc2d07aee5fe0d4252d4f1e
Noble-Mushtak <noblemushtak.public@gmail.com>
2021-12-23 13:09:29 -0500

    Got rid of some hardcoded numbers

--- 2021/day23/main.scala
+++ 2021/day23/main.scala
@@ -1,9 +1,11 @@
 import scala.io.Source
 import scala.collection.mutable._
 import scala.util.control.Breaks._
-object main {    
-    def base5ToInt(repr: Array[Int]): Long = {
-        repr.foldLeft(0L)((res, num) => 5L*res+num.toLong)
+object main {
+    val NUM_ROOMS = 4
+    
+    def baseToInt(base: Int, repr: Array[Int]): Long = {
+        repr.foldLeft(0L)((res, num) => base.toLong*res+num.toLong)
     }
 
     def longPow(base: Long, exp: Long): Long = {
@@ -18,8 +20,8 @@ object main {
         ans
     }
 
-    def getBase5Dig(num: Long, idx: Long): Int = {
-        ((num/longPow(5, idx)) % 5).toInt
+    def getBaseDig(base: Int, num: Long, idx: Long): Int = {
+        ((num/longPow(base, idx)) % base).toInt
     }
     
     def formatDig(dig: Int): Char = {
@@ -33,12 +35,12 @@ object main {
         ans += "\n"
 
         ans += "#"
-        ans += formatDig(getBase5Dig(node, numRooms*numSpacesPerRoom))
+        ans += formatDig(getBaseDig(numRooms+1, node, numRooms*numSpacesPerRoom))
         for (j <- Range(0, numRooms+1)) {
             if (j > 0) ans += "."
-            ans += formatDig(getBase5Dig(node, numRooms*numSpacesPerRoom+j+1))
+            ans += formatDig(getBaseDig(numRooms+1, node, numRooms*numSpacesPerRoom+j+1))
         }
-        ans += formatDig(getBase5Dig(node, numRooms*numSpacesPerRoom+numRooms+2))
+        ans += formatDig(getBaseDig(numRooms+1, node, numRooms*numSpacesPerRoom+numRooms+2))
         ans += "#\n"
 
         for (i <- Range(0, numSpacesPerRoom)) {
@@ -46,7 +48,7 @@ object main {
             else ans += "  "
             for (j <- Range(0, numRooms)) {
                 ans += "#"
-                ans += formatDig(getBase5Dig(node, numSpacesPerRoom*j+i))
+                ans += formatDig(getBaseDig(numRooms+1, node, numSpacesPerRoom*j+i))
             }
             ans += "#"
             if (i == 0) ans += "##"
@@ -96,42 +98,42 @@ object main {
                 var curTup: Option[(Int, Long, Int)] = None
                 for (j <- Range(0, numSpacesPerRoom)) { breakable {
                     if (curTup != None) break()
-                    val curDig = getBase5Dig(curNode, numSpacesPerRoom*i+j)
+                    val curDig = getBaseDig(numRooms+1, curNode, numSpacesPerRoom*i+j)
                     if (curDig > 0) {
-                        curTup = Some((curDig, longPow(5, numSpacesPerRoom*i+j), j+1))
+                        curTup = Some((curDig, longPow(numRooms+1, numSpacesPerRoom*i+j), j+1))
                         break()
                     }
                 } }
                 val (curDig, curPow, dst) = curTup.getOrElse(break())
                 var j = i+1
-                while ((j >= 0) && (getBase5Dig(curNode, numRooms*numSpacesPerRoom+j) == 0)) {
-                    val newNode = curNode-curDig*curPow+curDig*longPow(5, numRooms*numSpacesPerRoom+j)
+                while ((j >= 0) && (getBaseDig(numRooms+1, curNode, numRooms*numSpacesPerRoom+j) == 0)) {
+                    val newNode = curNode-curDig*curPow+curDig*longPow(numRooms+1, numRooms*numSpacesPerRoom+j)
                     val movement = dst+1+2*(i+1-j)-(if (j == 0) { 1 } else { 0 })
                     addVertex(dijkQ, dsts, prev, curNode, newNode, curDst+longPow(10, curDig-1)*movement)
                     j -= 1
                 }
                 j = i+2
-                while ((j < 7) && (getBase5Dig(curNode, numRooms*numSpacesPerRoom+j) == 0)) {
-                    val newNode = curNode-curDig*curPow+curDig*longPow(5, numRooms*numSpacesPerRoom+j)
-                    val movement = dst+1+2*(j-i-2)-(if (j == 6) { 1 } else { 0 })
+                while ((j < numRooms+3) && (getBaseDig(numRooms+1, curNode, numRooms*numSpacesPerRoom+j) == 0)) {
+                    val newNode = curNode-curDig*curPow+curDig*longPow(numRooms+1, numRooms*numSpacesPerRoom+j)
+                    val movement = dst+1+2*(j-i-2)-(if (j == numRooms+2) { 1 } else { 0 })
                     addVertex(dijkQ, dsts, prev, curNode, newNode, curDst+longPow(10, curDig-1)*movement)
                     j += 1
                 }
             } }
 
-            for (i <- Range(0, 7)) { breakable {
-                val curDig = getBase5Dig(curNode, numRooms*numSpacesPerRoom+i)
+            for (i <- Range(0, numRooms+3)) { breakable {
+                val curDig = getBaseDig(numRooms+1, curNode, numRooms*numSpacesPerRoom+i)
                 if (curDig == 0) break()
                 var curTup: Option[(Int, Int)] = None
                 for (j <- Range(0, numSpacesPerRoom)) { breakable {
                     if (curTup != None) break()
                     for (k <- Range(0, j)) {
-                        if (getBase5Dig(curNode, numSpacesPerRoom*curDig-k-1) != curDig) {
+                        if (getBaseDig(numRooms+1, curNode, numSpacesPerRoom*curDig-k-1) != curDig) {
                             break()
                         }
                     }
                     for (k <- Range(j, numSpacesPerRoom)) {
-                        if (getBase5Dig(curNode, numSpacesPerRoom*curDig-k-1) != 0) {
+                        if (getBaseDig(numRooms+1, curNode, numSpacesPerRoom*curDig-k-1) != 0) {
                             break()
                         }
                     }
@@ -142,14 +144,14 @@ object main {
                 var spaceExists = true
                 val curIter = if (i > curDig) { Range(curDig+1, i) } else { Range(i+1, curDig+1) }
                 for (j <- curIter) { breakable {
-                    if (getBase5Dig(curNode, numRooms*numSpacesPerRoom+j) != 0) {
+                    if (getBaseDig(numRooms+1, curNode, numRooms*numSpacesPerRoom+j) != 0) {
                         spaceExists = false
                         break()
                     }
                 } }
                 if (!spaceExists) break()
-                val newNode = curNode-curDig*longPow(5, numRooms*numSpacesPerRoom+i)+curDig*longPow(5, nextDig)
-                val movement = dst+1+2*(if (i > curDig) { i-curDig-1 } else { curDig-i })-(if ((i == 0) || (i == 6)) { 1 } else { 0 })
+                val newNode = curNode-curDig*longPow(numRooms+1, numRooms*numSpacesPerRoom+i)+curDig*longPow(numRooms+1, nextDig)
+                val movement = dst+1+2*(if (i > curDig) { i-curDig-1 } else { curDig-i })-(if ((i == 0) || (i == numRooms+2)) { 1 } else { 0 })
                 addVertex(dijkQ, dsts, prev, curNode, newNode, curDst+longPow(10, curDig-1)*movement)
             } }
         } }
@@ -160,11 +162,11 @@ object main {
         val puzzleInput = Source.fromFile(args(0)).getLines().toVector
         val printPath = (args.length >= 2) && (args(1) == "-printpath")
         
-        val startNode1 = base5ToInt(Array(puzzleInput(3)(9), puzzleInput(2)(9), puzzleInput(3)(7), puzzleInput(2)(7), puzzleInput(3)(5), puzzleInput(2)(5), puzzleInput(3)(3), puzzleInput(2)(3)).map(ch => ch.toInt-'A'.toInt+1).toArray)
-        val endNode1 = base5ToInt(Array(4, 4, 3, 3, 2, 2, 1, 1))
-        println(f"Part 1: ${runDijk(4, 2, startNode1, endNode1, printPath)}")
-        val startNode2 = base5ToInt(Array(puzzleInput(3)(9), 'C', 'A', puzzleInput(2)(9), puzzleInput(3)(7), 'A', 'B', puzzleInput(2)(7), puzzleInput(3)(5), 'B', 'C', puzzleInput(2)(5), puzzleInput(3)(3), 'D', 'D', puzzleInput(2)(3)).map(ch => ch.toInt-'A'.toInt+1).toArray)
-        val endNode2 = base5ToInt(Array(4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1))
+        val startNode1 = baseToInt(NUM_ROOMS+1, Array(puzzleInput(3)(9), puzzleInput(2)(9), puzzleInput(3)(7), puzzleInput(2)(7), puzzleInput(3)(5), puzzleInput(2)(5), puzzleInput(3)(3), puzzleInput(2)(3)).map(ch => ch.toInt-'A'.toInt+1).toArray)
+        val endNode1 = baseToInt(NUM_ROOMS+1, Array(4, 4, 3, 3, 2, 2, 1, 1))
+        println(f"Part 1: ${runDijk(NUM_ROOMS, 2, startNode1, endNode1, printPath)}")
+        val startNode2 = baseToInt(NUM_ROOMS+1, Array(puzzleInput(3)(9), 'C', 'A', puzzleInput(2)(9), puzzleInput(3)(7), 'A', 'B', puzzleInput(2)(7), puzzleInput(3)(5), 'B', 'C', puzzleInput(2)(5), puzzleInput(3)(3), 'D', 'D', puzzleInput(2)(3)).map(ch => ch.toInt-'A'.toInt+1).toArray)
+        val endNode2 = baseToInt(NUM_ROOMS+1, Array(4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1))
         println(f"Part 2: ${runDijk(4, 4, startNode2, endNode2, printPath)}")
     }
 }
